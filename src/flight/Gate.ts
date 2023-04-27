006a6a8bdf37857076b9ef6fccba8006d1be6667
soknoeurn-srun <soknoeurn.srun@student.passerellesnumeriques.org>
2023-04-27 14:55:51 +0700

    create flight relational info

---
+++ src/flight/Gate.ts
@@ -0,0 +1,6 @@
+export class Gate {
+  private gate: string;
+  constructor(gate: string) {
+    this.gate = gate;
+  }
+}
---
+++ src/flight/Route.ts
@@ -0,0 +1,9 @@
+export class Route {
+  private departure: string;
+  private arrival: string;
+
+  constructor(departure: string, arrival: string) {
+    this.departure = departure;
+    this.arrival = arrival;
+  }
+}
